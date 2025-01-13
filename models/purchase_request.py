c1cd71f218a079ac11e248b7133d7c958796d7ef
Sarif Hidayatulloh <100738114+PieceofShid@users.noreply.github.com>
2025-01-13 08:54:04 +0700

    fix: show reject button

--- models/purchase_request.py
+++ models/purchase_request.py
@@ -22,6 +22,19 @@ class PurchaseRequest(models.Model):
     
     is_approval_active = fields.Boolean(string="Is Approval Active", compute=_get_approval_config_status)
 
+    def _show_rejected_button(self):
+        for purchase in self:
+            company_unit_request = purchase.requested_by.company_unit_id
+            company_unit_users   = self.env.user.company_unit_id
+            check_user_groups    =  self.env.user.has_group("purchase_request.group_purchase_request_manager")
+            # if company_unit_request.id == company_unit_users.id and purchase.state not in ('pending_release', 'waiting_release', 'waiting_complete', 'approved'):
+            if company_unit_request.id == company_unit_users.id and purchase.state in ('pending_release', 'waiting_release', 'waiting_complete'):
+                purchase.is_show_rejected_button = True
+            else:
+                purchase.is_show_rejected_button = False
+                
+    is_show_rejected_button = fields.Boolean(string="Show Reject Buttom", compute=_show_rejected_button)
+
     @api.onchange('requested_by')
     def _get_default_company_uni(self):
         for request in self:
